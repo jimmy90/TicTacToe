3eb6f7d43ac7bac6ab5182491988c98f337697db
Jordan <jordansmagghe@live.fr>
2013-10-13 14:52:17 +0200

    ajout de message pour faciliter le déroulement du jeu

--- main.adb
+++ main.adb
@@ -1,17 +1,17 @@
 With Ada.Text_Io,Ada.Integer_Text_IO;USE Ada.Text_IO,Ada.Integer_Text_IO;
-with Cellules; 
-with Grille; 
+with Cellules;
+with Grille;
 With Jeu;
-procedure main is 
+procedure main is
 	a:integer;
 begin
-	Put_Line("Choisissez le nombre de joueur");
+	Put_Line("Choisissez le nombre de joueur (1) pour jouer avec l'IA ou (2) pour deux joueurs");
 	loop --Une boucle afin que le joueur puisse ressaisir un nombre correct de joueur au cas oû il se tromperait.
 		Get(A);
 		if a=1 then
 			Put_Line("L'IA est en cours de construction");
 			exit;  -- Il sort prématurement de la boucle si et seulement si il tape un nombre correct
-		else if a=2 then 
+		else if a=2 then
 				Put_Line("Le jeu commence");
 				Skip_line; --Je vide le buffer d'entrée sortie avant d'appeller la procédure Jeu
 					-- sinon l'entier designant le nombre de joueur sera compter au lancement comme une mauvaise colonne.
